71d84846a4166dbad60f29440a61a6dca469a3e4
unknown <kahveci.serkan@hotmail.com>
2023-11-08 14:22:13 +0300

    First Commit

---
+++ GitPuller/CommandExecuter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPuller
+{
+    public class CommandExecuter
+    {
+        public string Execute(string command)
+        {           
+            var processStartInfo = new ProcessStartInfo("powershell.exe", command);
+
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.UseShellExecute = false;
+
+            var process = new Process();
+            process.StartInfo = processStartInfo;
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+
+            Console.WriteLine(output);
+
+            return output;
+        }
+    }
+}
---
+++ GitPuller/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace GitPuller
+{
+    internal class Program
+    {
+        static string accessToken;
+
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            accessToken = configuration.GetSection("Git")["AccessToken"];
+
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                Console.Write("Enter Git Access Token: ");
+                accessToken = Console.ReadLine();
+            }
+
+            var exec = new CommandExecuter();
+
+            // Example usage of the provided access token
+            var gitCloneCommand = $"git clone https://{accessToken}@github.com/your/repository.git";
+            var result = exec.Execute(gitCloneCommand);
+
+            Console.WriteLine(result);
+
+            // Now you can use 'accessToken' for other Git commands
+
+            Console.ReadLine();
+        }
+    }
+}
