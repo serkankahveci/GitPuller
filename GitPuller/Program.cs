25780a1f30ff4ddc07876b9592ca0ac842382ef5
unknown <kahveci.serkan@hotmail.com>
2023-11-29 16:46:37 +0300

    Repo döngüsü

--- GitPuller/Program.cs
+++ GitPuller/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.Extensions.Configuration;
-using System.IO;
-using System.Linq;
+using Octokit;
 
 namespace GitPuller
 {
@@ -12,9 +12,9 @@ namespace GitPuller
         static void Main(string[] args)
         {
             var config = new ConfigurationBuilder()
-                   .SetBasePath(@"C:\Users\XYZ\source\repos\GitPuller\GitPuller")
-                   .AddJsonFile("appsettings.json")
-                   .Build();
+                .SetBasePath(@"C:\Users\XYZ\source\repos\GitPuller\GitPuller")
+                .AddJsonFile("appsettings.json")
+                .Build();
 
             accessToken = config.GetSection("Git")["AccessToken"];
 
@@ -26,63 +26,34 @@ namespace GitPuller
 
             var exec = new CommandExecuter();
 
-            Console.WriteLine("-------");
-            var repoCommand = $"curl \"https://github.com/serkankahveci?tab=repositories/?type=all\"";
-            var repoResult = exec.Execute(repoCommand);
+            var github = new GitHubClient(new ProductHeaderValue("GitPuller"));
+            github.Credentials = new Credentials(accessToken);
 
-            Console.WriteLine(repoResult);
+            var repositories = github.Repository.GetAllForCurrent().Result;
 
-            Console.WriteLine("-------");
-
-            foreach (var item in repoResult)
+            foreach (var repository in repositories)
             {
-                var takeBranchList = "git branch -a";
-                var branchListResult = exec.Execute(takeBranchList);
-                Console.WriteLine(branchListResult);
+                Console.WriteLine("Repository Name: " + repository.Name);
+                Console.WriteLine("-------");
 
+                var branches = github.Repository.Branch.GetAll(repository.Id).Result;
 
-
-                var branchNames = branchListResult
-                    .Split('\n', (char)StringSplitOptions.RemoveEmptyEntries)
-                    .Select(branch => branch.Trim('*').Trim())
-                    .ToList();
-
-
-
-                foreach (var branchName in branchNames)
+                foreach (var branch in branches)
                 {
-                    Console.WriteLine($"{branchName}");
-                    //Console.WriteLine("Stashing ...");
-                    //var gitStashCommand = "git stash save -S 'New Save'";
-                    //var stashResult = exec.Execute(gitStashCommand);
-                    //Console.WriteLine(stashResult);
+                    Console.WriteLine($"{branch.Name}");
 
-                    //Console.WriteLine("TEST");
-                    //var test = "git sdasdas";
-                    //var testResult = exec.Execute(test);
-                    //Console.WriteLine(testResult);
-                    //Console.WriteLine("TEST");
+                    Console.WriteLine("TEST");
+                    var test = "git sdasdas";
+                    var testResult = exec.Execute(test);
+                    Console.WriteLine(testResult);
+                    Console.WriteLine("TEST");
 
-                    //Console.WriteLine("Cloning ...");
-                    //var gitCloneCommand = $"git clone https://{accessToken}@github.com/serkankahveci/GitPuller.git";
-                    //var cloneResult = exec.Execute(gitCloneCommand);
-                    //Console.WriteLine(cloneResult);
-
-                    //Console.WriteLine("Git clone completed.");
-
-                    //Console.WriteLine("Pulling ...");
-                    //var gitPullCommand = "git pull origin master";
-                    //var pullResult = exec.Execute(gitPullCommand);
-                    //Console.WriteLine(pullResult);
-
-                    //Console.WriteLine("Popping ...");
-                    //var gitStashPop = "git stash pop stash@{0}";
-                    //var popResult = exec.Execute(gitStashPop);
-                    //Console.WriteLine(popResult);
+                    Thread.Sleep(4000);
                 }
-                Console.ReadLine();
-                //maNga nasıl birinci olamadı yaw
+
+                Console.WriteLine("-------");
             }
+                Console.ReadLine();
         }
     }
 }
